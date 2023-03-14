43233141c9332ebc6f2c7bca023f0b4c2f95a41c
lonely <117298651+lonely43@users.noreply.github.com>
2023-03-14 20:03:12 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function money(){
+   const random = Math.floor(Math.random() * 3);
+   if(random == 1)
+      alert("Dont touch me")
+   else if(random == 2)
+      alert("Stop it")
+   else 
+      alert("Don't do it again")
+}
+
+
+/*
+function money(){
+   let con = confirm("You send me money?")
+   if (con)
+      alert("Thank you!) 4967 5019 3123 1253")
+   else
+      alert("why?(")
+   }
+*/
